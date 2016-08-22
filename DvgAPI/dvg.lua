6c640adbab57a41f71f4844bed54c821f8b6719a
Dante van Gemert <dantevg006@gmail.com>
2016-08-22 22:10:33 +0200

    Update to 2.20.1

--- DvgAPI/dvg.lua
+++ DvgAPI/dvg.lua
@@ -3,17 +3,45 @@
       Dvg API
       by DvgCraft
 
-      VERSION 2.17.2
-      DATE    23-06-2016
+      DATE    22-08-2016
       GITHUB  github.com/Dantevg/DvgApps (/tree/master/DvgAPI)
 
 ]]--
 
-version = "2.17.2"
+version = "2.20.1"
 
 
 sides = { "right", "left", "top", "bottom", "back", "front" }
-colorToName = {
+
+
+
+--     ██████  ██████  ██       ██████  ██████  ███████
+--    ██      ██    ██ ██      ██    ██ ██   ██ ██
+--    ██      ██    ██ ██      ██    ██ ██████  ███████
+--    ██      ██    ██ ██      ██    ██ ██   ██      ██
+--     ██████  ██████  ███████  ██████  ██   ██ ███████
+
+
+color = {}
+color.complementary = {
+  [1] = 32768,
+  [2] = 2048,
+  [4] = 8192,
+  [8] = 16,
+  [16] = 8,
+  [32] = 1024,
+  [64] = 512,
+  [128] = 256,
+  [256] = 128,
+  [512] = 16384,
+  [1024] = 32,
+  [2048] = 16384,
+  [4096] = 2048,
+  [8192] = 4,
+  [16384] = 512,
+  [32768] = 1,
+}
+color.colorToName = {
   [1] = "white",
   [2] = "orange",
   [4] = "magenta",
@@ -31,7 +59,29 @@ colorToName = {
   [16384] = "red",
   [32768] = "black",
 }
-nameToColor = {
+color.colorToHex = {
+  [1] = "F0F0F0",
+  [2] = "F2B233",
+  [4] = "E57FD8",
+  [8] = "99B2F2",
+  [16] = "DEDE6C",
+  [32] = "7FCC19",
+  [64] = "F2B2CC",
+  [128] = "4C4C4C",
+  [256] = "999999",
+  [512] = "4C99B2",
+  [1024] = "B266E5",
+  [2048] = "3366CC",
+  [4096] = "7F664C",
+  [8192] = "57A64E",
+  [16384] = "CC4C4C",
+  [32768] = "191919",
+}
+color.colorToRgb = function( color )
+  return dvg.color.hexToRgb( dvg.color.colorToHex[color] )
+end
+
+color.nameToColor = {
   ["white"] = 1,
   ["orange"] = 2,
   ["magenta"] = 4,
@@ -49,8 +99,142 @@ nameToColor = {
   ["red"] = 16384,
   ["black"] = 32768,
 }
-toName = colorToName -- Deprecated
-toColor = nameToColor -- Deprecated
+
+color.hexToColor = {
+  ["F0F0F0"] = 1,
+  ["F2B233"] = 2,
+  ["E57FD8"] = 4,
+  ["99B2F2"] = 8,
+  ["DEDE6C"] = 16,
+  ["7FCC19"] = 32,
+  ["F2B2CC"] = 64,
+  ["4C4C4C"] = 128,
+  ["999999"] = 256,
+  ["4C99B2"] = 512,
+  ["B266E5"] = 1024,
+  ["3366CC"] = 2048,
+  ["7F664C"] = 4096,
+  ["57A64E"] = 8192,
+  ["CC4C4C"] = 16384,
+  ["191919"] = 32768,
+}
+color.hexToRgb = function( hex )
+  if type( hex ) == "number" then
+    hex = dvg.decToBase( hex, 16 )
+  elseif type( hex ) == "string" then
+    if not tonumber( hex, 16 ) then
+      error( 2, "Expected string/number ([0-9A-F])", 2 )
+    else
+      hex = dvg.decToBase( tonumber(hex, 16), 16 )
+    end
+  else
+    error( 2, "Expected string/number ([0-9A-F])", 2 )
+  end
+
+  local r = tonumber( hex:sub( 1,2 ), 16 )
+  local g = tonumber( hex:sub( 3,4 ), 16 )
+  local b = tonumber( hex:sub( 5,6 ), 16 )
+
+  return r, g, b
+end
+
+color.rgbToHex = function( r, g, b )
+  if not tonumber( r ) or not tonumber( g ) or not tonumber( b ) then
+    error( 2, "Expected number, number, number or str/num", 2 )
+  end
+
+  r = dvg.decToBase( tonumber(r), 16 )
+  g = dvg.decToBase( tonumber(g), 16 )
+  b = dvg.decToBase( tonumber(b), 16 )
+
+  local hex = ""
+  hex = hex..dvg.fill( r, -2, "0" )
+  hex = hex..dvg.fill( g, -2, "0" )
+  hex = hex..dvg.fill( b, -2, "0" )
+
+  return hex
+end
+color.rgbToColor = function( r,g,b )
+  return dvg.color.hexToColor[ dvg.color.rgbToHex( r,g,b ) ]
+end
+
+-- Following hsl functions modified from Github (https://goo.gl/5MBnPw)
+color.rgbToHsl = function( r, g, b )
+  r, g, b = r / 255, g / 255, b / 255
+
+  local max, min = math.max(r, g, b), math.min(r, g, b)
+
+  local l = (max + min) / 2
+
+  if max == min then
+    h, s = 0, 0 -- achromatic
+  else
+    local d = max - min
+    if l > 0.5 then s = d / (2 - max - min) else s = d / (max + min) end
+    if max == r then
+      h = (g - b) / d
+      if g < b then h = h + 6 end
+    elseif max == g then h = (b - r) / d + 2
+    elseif max == b then h = (r - g) / d + 4
+    end
+    h = h / 6
+  end
+
+  return h*360, s, l
+end
+
+color.hslToRgb = function( h, s, l )
+  local r, g, b = 0,0,0
+  h = h/360
+
+  if s == 0 then
+    r, g, b = l, l, l -- achromatic
+  else
+    function hue2rgb(p, q, t)
+      if t < 0   then t = t + 1 end
+      if t > 1   then t = t - 1 end
+      if t < 1/6 then return p + (q - p) * 6 * t end
+      if t < 1/2 then return q end
+      if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+      return p
+    end
+
+    local q = 0
+    if l < 0.5 then q = l * (1 + s) else q = l + s - l * s end
+    local p = 2 * l - q
+
+    r = hue2rgb(p, q, h + 1/3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1/3)
+  end
+
+  return r*255, g*255, b*255
+end
+
+color.nearest = function( h,s,l )
+  local diff = {}
+  local c = {}
+  for i = 0, 15 do
+    c[i] = { dvg.color.rgbToHsl( dvg.color.hexToRgb(dvg.color.colorToHex[2^i]) ) }
+  end
+
+  for color, values in ipairs( c ) do
+    diff[color] = math.abs( c[color][1] - h )
+    diff[color] = diff[color] + math.abs( c[color][2] - s )
+    diff[color] = diff[color] + math.abs( c[color][3] - l )
+  end -- End for color, values in c
+
+  return table.unpack( c[ dvg.nearest(diff, 0) ] )
+end
+
+
+
+--     ██████  ██████   █████  ██████  ██   ██ ██  ██████  █████  ██
+--    ██       ██   ██ ██   ██ ██   ██ ██   ██ ██ ██      ██   ██ ██
+--    ██   ███ ██████  ███████ ██████  ███████ ██ ██      ███████ ██
+--    ██    ██ ██   ██ ██   ██ ██      ██   ██ ██ ██      ██   ██ ██
+--     ██████  ██   ██ ██   ██ ██      ██   ██ ██  ██████ ██   ██ ███████
+
 
 function setTextColor( color )
   term.setTextColor( color )
@@ -65,6 +249,7 @@ function bg( color )
   term.setBackgroundColor( color )
   term.clear()
 end
+
 function writeColor( string, color )
   term.setTextColor( color )
   write( string )
@@ -86,6 +271,7 @@ function printPos( string, x,y )
   print( string )
 end
 
+
 function center( text, y )
   local curX, curY = term.getCursorPos()
   local w, _ = term.getSize()
@@ -96,12 +282,14 @@ function center( text, y )
 end
 
 function fill( text, to, char )
-  if type( text ) ~= "string" or type( to ) ~= "number" then
-    error( "Expected string, number [,string]" )
+  if not tostring( text ) or type( to ) ~= "number" then
+    error( 2, "Expected string, number [,string]" )
   end
-  if char and type( char ) ~= "string" then
-    error( "Expected string, number [,string]" )
+  if char and not tostring( char ) then
+    error( 2, "Expected string, number [,string]" )
   end
+  text = tostring( text )
+  char = tostring( char )
   local inv = false
   if to < 0 then
     inv = true
@@ -131,7 +319,7 @@ end
 
 function scroll( text, height, offset )
   if type( text ) ~= "table" or type( height ) ~= "number" or type( offset ) ~= "number" then
-    error( "Expected table text, number height, number offset" )
+    error( 2, "Expected table text, number height, number offset" )
   end
   for i = 1, offset do
     if #text <= height then break end
@@ -143,9 +331,18 @@ function scroll( text, height, offset )
   return text
 end
 
+
+
+--    ███████ ██    ██ ███    ██  ██████ ████████ ██  ██████  ███    ██ ██ ███    ██  ██████
+--    ██      ██    ██ ████   ██ ██         ██    ██ ██    ██ ████   ██ ██ ████   ██ ██
+--    █████   ██    ██ ██ ██  ██ ██         ██    ██ ██    ██ ██ ██  ██ ██ ██ ██  ██ ██   ███
+--    ██      ██    ██ ██  ██ ██ ██         ██    ██ ██    ██ ██  ██ ██ ██ ██  ██ ██ ██    ██
+--    ██       ██████  ██   ████  ██████    ██    ██  ██████  ██   ████ ██ ██   ████  ██████
+
+
 function read( input, exitEvent, exitParam, exitVal )
   if ( input and type(input) ~= "string" ) or ( exitEvent and type( exitEvent ) ~= "string" ) or (exitParam and (type(exitParam)~="number" or exitParam>5) ) then
-    error( "Expected [string input [,string exitEvent [,number exitParam, any exitVal]]" )
+    error( 2, "Expected [string input [,string exitEvent [,number exitParam, any exitVal]]" )
   end -- Input: string,  exitEvent: string,  exitParam: number < 6,  exitVal: any
 
   local input = input or ""
@@ -189,7 +386,7 @@ end
 
 function checkupdate( url, current )
   if type( url ) ~= "string" then
-    error( "Expected string URL, [string version]" )
+    error( 2, "Expected string URL, [string version]" )
   end
 
   http.request( url )
@@ -214,7 +411,7 @@ end
 
 function compareVersions( new, old )
   if not new or not old or type( new ) ~= "string" or type( old ) ~= "string" then
-    error( "Expected string new, string old" )
+    error( 2, "Expected string new, string old" )
   end
   local t1 = {}
   local t2 = {}
@@ -238,27 +435,52 @@ function readFile( path, tbl )
   file.close()
   return contents
 end
-function writeFile( path, contents, mode )
+function writeFile( path, contents, mode, tbl )
+  if not tbl then
+    if type(mode) == "boolean" then tbl = mode mode = nil end
+  end
   if fs.isReadOnly( path ) then return nil end
   local file = fs.open( path, mode and mode or "w" )
-  file.write( contents )
+  file.write( tbl and textutils.serialize(contents) or contents )
   file.close()
   return true
 end
 
 
+function openRednet()
+  local modems = { peripheral.find( "modem" ) }
+  for _, modem in pairs( modems ) do
+    if modem.isWireless() then
+      modem.open( os.getComputerID() ) -- Same as rednet.open()
+      modem.open( 65535 ) -- For broadcasting
+      return true
+    end
+    return false, "No wireless modems"
+  end
+  return false, "No modems"
+end
+
+
+
+--    ██    ██  █████  ██      ██    ██ ███████ ███████
+--    ██    ██ ██   ██ ██      ██    ██ ██      ██
+--    ██    ██ ███████ ██      ██    ██ █████   ███████
+--     ██  ██  ██   ██ ██      ██    ██ ██           ██
+--      ████   ██   ██ ███████  ██████  ███████ ███████
+
+
 function decToBase( n, base )
   if type( n ) ~= "number" or (base and type( base ) ~= "number") then
-    error( "Expected number [, base]", 2 )
+    error( 2, "Expected number [, base]", 2 )
   elseif base < 2 or base > 36 then
-    error( "Base can only be <= 10", 2 )
+    error( 2, "Base can only be >2 and <36", 2 )
   end
   if base == 10 then
     return tostring( num )
   elseif base == 8 then
-    return string.format( tostring(n), "%o" )
+    return string.format( "%o", tostring(n) )
   elseif base == 16 then
-    return string.format( tostring(n), "%X" )
+    return string.format( "%X", tostring(n) )
   end
 
   local digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -278,9 +500,9 @@ function decToBase( n, base )
 end
 function baseToDec( n, base )
   if (type( n ) ~= "string" and type( n ) ~= "number") or (base and type( base ) ~= "number") then
-    error( "Expected number [, base]", 2 )
+    error( 2, "Expected number [, base]", 2 )
   elseif base < 2 or base > 36 then
-    error( "Base can only be <= 10", 2 )
+    error( 2, "Base can only be <= 10", 2 )
   end
   return tonumber( n, base )
 end
@@ -329,7 +551,7 @@ end
 
 function inAny( checkIn, checkFor )
   if checkIn == nil or type( checkIn ) ~= "table" or checkFor == nil then
-    error( "Expected table, value" )
+    error( 2, "Expected table, value" )
   end
   for i = 1, #checkIn do
     if checkIn[i] == checkFor then
@@ -339,24 +561,31 @@ function inAny( checkIn, checkFor )
   return false
 end
 
-
-function openRednet()
-  local modems = { peripheral.find( "modem" ) }
-  for _, modem in pairs( modems ) do
-    if modem.isWireless() then
-      modem.open( os.getComputerID() ) -- Same as rednet.open()
-      modem.open( 65535 ) -- For broadcasting
-      return true
-    end
-    return false, "No wireless modems"
+function nearest( values, value )
+  if type( values ) ~= "table" or type( value ) ~= "number" then
+    error( 2, "Expected table, number" )
   end
-  return false, "No modems"
+
+  local index, difference
+  for k, v in ipairs(values) do
+    if not difference or (math.abs(value-v) < difference) then
+      index, difference = k, math.abs(value-v)
+    end
+  end
+  return index, values[index], difference
 end
+
+
+--    ███    ███ ██ ███████  ██████
+--    ████  ████ ██ ██      ██
+--    ██ ████ ██ ██ ███████ ██
+--    ██  ██  ██ ██      ██ ██
+--    ██      ██ ██ ███████  ██████
 
 
 function compass()
   if not turtle then
-    error( "This function is only for turtles" )
+    error( 2, "This function is only for turtles" )
   end
   local x1, y1, z1 = gps.locate()
   if not x1 then return nil end
